21746c89e9bb8a284f8761482a0ff5e75b0cf0cf
Adnane Ouassari <Adnaneouassari@gmail.com>
2024-01-18 16:47:34 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 let counting = setInterval(updateNumber);
 let count = 0;
-let number = document.querySelector('#number')
+let number = document.querySelector('#number');
 function updateNumber() {
     number.innerText = ++count
     if (count == 1000) {
